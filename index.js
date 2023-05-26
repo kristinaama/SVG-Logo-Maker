51295a9ba0f3e03c99ad934666d68f806db75501
Kristina <kristina@adsl-104-177-149-164.lightspeed.sntcca.sbcglobal.net>
2023-05-26 11:48:43 -0700

    created shapes.js file, edited functions in generateLogo.js, and edited index.js

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 
+// Import the node packages and directories required
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateLogo = require("./lib/generateLogo");
+//const { Triangle, Square, Circle } = require("lib/shapes.js");
+//const generateLogo = require("./utils/generateLogo.js");
 
-// Create an array of questions for user input and generate a ReadMe file
+// userResponses() function will prompt the user to answer questions in the command line and generate a logo
 function userResponses() {
     inquirer
         .prompt([
         {
             type: 'input',
             name: 'text',
-            message: 'Enter your logo (up to three letters):'
+            message: 'Enter your logo text (up to three letters):'
         },
         {
             type: 'input',
             name: 'textColor',
-            message: 'What color would you like the text to be:'
+            message: 'What color would you like the text to be (enter color keyword or hexadecimal number):'
         },
         {
             type: 'input',
             name: 'shape',
-            message: 'What shape would you like the logo to be (circle, triangle, or square):'
+            message: 'What shape would you like the logo to be (circle, triangle, or square):',
+            choices: ['circle', 'triangle', 'square']
         },
         {
             type: 'input',
             name: 'shapeColor',
-            message: 'What color would you like the logo shape to be::'
+            message: 'What color would you like the logo shape to be(enter color keyword or hexadecimal number):'
         }
-    ]) .then (function(results) {
-        const outputLogo = generateLogo(results);
-        fs.writeFile("GeneratedLogo.svg", outputLogo, (error) => {
-            if (error) {
-                console.error(error);
-            } else {
-                console.log("Success!");
-            }
-        })
-    })
-    };
+         ]) .then(() => console.log("logo generated!"));
+    // ]) .then (function(results) {
+    //     fs.createLogo("Logo.svg", results, (error) => {
+    //         if (error) {
+    //             console.error(error);
+    //         } else {
+    //             console.log("Success, logo generated!");
+    //         }
+    //     })
+    // })
+     };
+
+// init() function will begin the userResponses() function
+function init() {
+    userResponses();
+}
     
-    // Create a function to initialize app
-    function init() {
-        responses();
-    }
-    
-    // Function call to initialize app
-    init();
+// The init() function will run when application starts
+init();
